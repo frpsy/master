11fe6c09926e3045dd397bf574089468b383a456
sangyeol.park <sangyeol.park@lge.com>
2024-02-25 16:34:20 +0900

    del logs

--- dev_cal.c
+++ dev_cal.c
@@ -1,15 +1,11 @@
 #include <stdio.h>
-/// @brief test function
-/// @param  void
-/// @return result of function
+
 int testfunction(void){
 	int i = 1;
 	printf("this is a number: %d\n", i);
 	return 0;
 }
-/// @brief main
-/// @param void
-/// @return void
+
 int main(void)
 {
 	for (int i = 1; i < 5; i++){
